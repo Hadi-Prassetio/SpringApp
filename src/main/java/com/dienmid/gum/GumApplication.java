9fbac09ccec65b86de33dedc05fc2d3da080b9d0
Hadi Prassetio <hadiprassetio516@gmail.com>
2023-08-06 17:42:47 +0700

    .

---
+++ src/main/java/com/dienmid/gum/GumApplication.java
@@ -0,0 +1,13 @@
+package com.dienmid.gum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dienmid/gum/GumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dienmid.gum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
